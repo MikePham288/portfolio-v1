ed3e6cb152be2d35f718eaba3586bdfad3b2e7e8
Mike Pham <lpham32@my.centennialcollege.ca>
2021-09-17 22:32:59 -0400

    Update basic contents and resume

--- server/config/app.js
+++ server/config/app.js
@@ -11,35 +11,32 @@ let cookieParser = require("cookie-parser");
 let logger = require("morgan");
 
 //modules for authentication
-let session = require('express-session');
-let passport = require('passport');
-let passportLocal = require('passport-local');
+let session = require("express-session");
+let passport = require("passport");
+let passportLocal = require("passport-local");
 let localStrategy = passportLocal.Strategy;
-let flash = require('connect-flash');
+let flash = require("connect-flash");
 
 // database setup
-let mongoose = require('mongoose');
-let DB = require('./db');
+let mongoose = require("mongoose");
+let DB = require("./db");
 
 //point mongoose to the DB URL
 mongoose.connect(DB.URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 let mongoDB = mongoose.connection;
-mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
-mongoDB.once('open', () => {
-  console.log('Connected to MongoDB...');
-})
-
+mongoDB.on("error", console.error.bind(console, "Connection Error:"));
+mongoDB.once("open", () => {
+	console.log("Connected to MongoDB...");
+});
 
 let indexRouter = require("../routes/index");
-let contactsRouter = require('../routes/contact');
+let contactsRouter = require("../routes/contact");
 let usersRouter = require("../routes/users");
-const {
-  default: sslRedirect
-} = require("heroku-ssl-redirect");
+const { default: sslRedirect } = require("heroku-ssl-redirect");
 
 let app = express();
 
@@ -52,19 +49,23 @@ app.set("view engine", "ejs"); //express -e
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+	express.urlencoded({
+		extended: false,
+	})
+);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../../public")));
 app.use(express.static(path.join(__dirname, "../../node_modules")));
 
 // setup express session
-app.use(session({
-  secret: "SomeSecret",
-  saveUninitialized: false,
-  resave: false
-}));
+app.use(
+	session({
+		secret: "SomeSecret",
+		saveUninitialized: false,
+		resave: false,
+	})
+);
 
 // Initialize flash
 app.use(flash());
@@ -75,8 +76,8 @@ app.use(passport.session());
 
 // passport user configuration
 
-//create a User Model Instance 
-let userModel = require('../models/user');
+//create a User Model Instance
+let userModel = require("../models/user");
 let User = userModel.User;
 
 // implement an User Authentication Strategy
@@ -86,27 +87,26 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use('/contact-list', contactsRouter);
+app.use("/contact-list", contactsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error", {
-    title: "Error"
-  });
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error", {
+		title: "Error",
+	});
 });
 
 module.exports = app;
